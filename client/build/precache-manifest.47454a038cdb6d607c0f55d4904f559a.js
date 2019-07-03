efb3b2c69ed68a2fb25a0d47cd7c3514fa39acd6
Orusa Ishtiaq <orusaishtiaq@gmail.com>
2019-07-03 17:08:11 -0400

    code ready to run

---
+++ client/build/precache-manifest.47454a038cdb6d607c0f55d4904f559a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc3f549ea9c664c6456ae77d3da259ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5cdd9a176f0a5f500b7",
+    "url": "/static/css/main.c93a207a.chunk.css"
+  },
+  {
+    "revision": "839af8bc866ed6a3acb2",
+    "url": "/static/js/2.086867ca.chunk.js"
+  },
+  {
+    "revision": "a5cdd9a176f0a5f500b7",
+    "url": "/static/js/main.55afcbf1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- client/src/components/ResultsContainer.js
+++ client/src/components/ResultsContainer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import BookResult from "../components/BookResult";
+import BookResult from "./BookResult";
 
 function ResultsContainer(props) {
   if (props.path === "/") {
